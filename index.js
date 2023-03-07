51dda7a84ad2717013d52b834fb58e75aa83d0ba
Hadi-Abdallah <95867955+Hadi-Abdallah@users.noreply.github.com>
2023-03-07 23:03:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const WebSocket = require('ws');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (socket) => {
+    console.log('WebSocket connected:', socket._socket.remoteAddress);
+  
+    socket.on('message', (message) => {
+        console.log('WebSocket message:', message);
+        const { event, data } = JSON.parse(message);
+        if (['new reservation', 'available queues change', 'deleted reservation'].includes(event)) {
+          wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify({ event, data }));
+            }
+          });
+        }
+      });
+
+  
+    socket.on('close', () => {
+      console.log('WebSocket closed');
+    });
+  });
+
+server.listen(8000, "0.0.0.0", () => {
+    const address = server.address();
+    console.log(`WebSocket server listening on ${address.address}:${address.port}`);
+  });
