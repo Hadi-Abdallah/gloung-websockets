be8ba960f998d3db9ebad9fab86b1e1ce2aa25c0
Hadi-Abdallah <95867955+Hadi-Abdallah@users.noreply.github.com>
2023-03-08 17:56:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors())
+
 const http = require('http');
-const WebSocket = require('ws');
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (socket) => {
-    console.log('WebSocket connected:', socket._socket.remoteAddress);
-  
-    socket.on('message', (message) => {
-        console.log('WebSocket message:', message);
-        const { event, data } = JSON.parse(message);
-        if (['new reservation', 'available queues change', 'deleted reservation'].includes(event)) {
-          wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({ event, data }));
-            }
-          });
-        }
-      });
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
-  
-    socket.on('close', () => {
-      console.log('WebSocket closed');
-    });
+io.on('connection', (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  socket.on('message', (message) => {
+    console.log('WebSocket message:', message);
+    if (['new reservation', 'available queues change', 'deleted reservation'].includes(message)) {
+      io.emit('message', message);
+    }
   });
 
-server.listen(3000, "0.0.0.0", () => {
-    const address = server.address();
-    console.log(`WebSocket server listening on ${address.address}:${address.port}`);
+  socket.on('disconnect', () => {
+    console.log(`Socket disconnected: ${socket.id}`);
   });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  const address = server.address();
+  console.log(`WebSocket server listening on ${address.address}:${address.port}`);
+});
